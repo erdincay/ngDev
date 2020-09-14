da3509869c7c945db26102845053bc8f7be766d3
Erdinc Ay <erdinc.ay@outlook.de>
2020-09-14 14:07:36 +0200

    food taken from foodservice

--- 01-Intro/second-ng/src/app/food/food.service.ts
+++ 01-Intro/second-ng/src/app/food/food.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { FoodItem } from './food-item.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FoodService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
-  food: FoodItem[] = [
-    { name: 'Pommes', price: 2 },
-    { name: 'Falafel', price: 2 },
-  ];
+  // food: FoodItem[] = [
+  //   { name: 'Pommes', price: 2 },
+  //   { name: 'Falafel', price: 2 },
+  // ];
 
   getFood(): Observable<FoodItem[]> {
-    return of(this.food);
+    return this.httpClient.get<FoodItem[]>(environment.url);
   }
 }
