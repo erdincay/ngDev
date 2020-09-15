7add6579edff9f421170b28c36a1eeea6789edff
Erdinc Ay <erdinc.ay@outlook.de>
2020-09-15 17:00:28 +0200

    add all

--- 01-Intro/third-ng/src/app/app.module.ts
+++ 01-Intro/third-ng/src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FoodContainerComponent } from './food/food-container/food-container.component';
+import { FoodListComponent } from './food/food-list/food-list.component';
+import { FoodEditComponent } from './food/food-edit/food-edit.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodContainerComponent,
+    FoodListComponent,
+    FoodEditComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
